35c5bf0227c2a4d79bc015e8b1227bca98ad2cb0
Umi007 <umihussain13@gmail.com>
2023-01-19 05:08:08 +0000

    Learnt how to create simple variables with types

--- Solidity Basics I/Variables & Types.sol
+++ Solidity Basics I/Variables & Types.sol
@@ -1,1 +1,23 @@
-init..
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract basicVariables {
+
+    uint chocolateBar = 2;
+    uint crisps = 3;
+    uint totalCost = chocolateBar + crisps;
+
+    // Different types for variables like other languages, for example bool and string!
+
+}
+
+contract useWallet {
+
+    // Create new variable that is an integer called 'wallet' and initialize it to 500
+    uint wallet = 500;
+    // Create a boolean called 'spend' and set it to false
+    bool spend = false;
+    // Create a string with the name notifySpend and add the string value 'You have spent money'
+    string notifySpend = "You have spent money";
+
+}
